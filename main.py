da389e6181b4a37e9b8e499c785fb495c8e65a3b
45uperman <orsuperman@gmail.com>
2023-12-26 09:27:22 -0600

    Added some code that displays the number of times each letter appears throughout the file.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-def wordcount(text):
-    words = text.split()
-    return words
+import string
+
 def gettext(name):
     book = f"books/{name}.txt"
     return open(book).read()
+def wordcount(text):
+    words = text.split()
+    return len(words)
+def lettercount(text, letter):
+    letters = text.split(letter)
+    return len(letters)
 
 filename = input("Which book would you like to look at?\n")
 filecontent = gettext(filename)
 print(f"There are {wordcount(filecontent)} words in {filename}!")
 more = input("Would you like to know more? [y/n]\n")
-if more == "y":
-    pass
-
+if more != "y":
+    exit
+letterenumeration = ""
+for i in string.ascii_lowercase:
+    letterenumeration = f"{letterenumeration}The letter '{i}' appears {lettercount(filecontent.lower(), i)} times.\n"
+print(letterenumeration)
