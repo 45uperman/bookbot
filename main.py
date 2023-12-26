f8827f2f67504d8b14a30bd7e9cc6004bab6e0a4
45uperman <orsuperman@gmail.com>
2023-12-26 08:54:21 -0600

    Removed the hello text from main.py and added the wordcount and gettext functions, as well as a few lines of code that take a file name as an input and return the number of words in that file.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello!")
+def wordcount(text):
+    words = text.split()
+    return words
+def gettext(name):
+    book = f"books/{name}.txt"
+    return open(book).read()
+
+filename = input("Which book would you like to look at?\n")
+filecontent = gettext(filename)
+print(f"There are {wordcount(filecontent)} words in {filename}!")
+more = input("Would you like to know more? [y/n]\n")
+if more == "y":
+    pass
+
